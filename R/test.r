188393b6734ec71c99774475bd41c76387ede81a
kazutan <kazu@kz-md.net>
2015-07-24 23:03:21 +0900

    change test.r

--- R/test.r
+++ R/test.r
@@ -3,3 +3,7 @@ library(ggplot2)
 ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length)) + 
   geom_point() + 
   theme_bw()
+
+ggplot(iris, aes(x = Petal.Width, y = Petal.Length)) + 
+  geom_point() + 
+  theme_bw()
